dc88ed829b2218a9b7426863b9f2d86f6500492e
Cleaver Barnes <cbarnes@cleaver.ca>
2020-11-11 17:34:34 -0500

    update app

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,50 +1,58 @@
-import { Utils } from "../app/Utils";
+import { Utils } from '../app/Utils'
 
-describe("Utils test suite", () => {
-  beforeEach(() => {
-    console.log("before each");
-  });
 
-  beforeAll(() => {
-    console.log("before all");
-  });
+describe('Utils test suite', () => {
 
-  test("first test", () => {
-    const result = Utils.toUpperCase("abc");
-    expect(result).toBe("ABC");
-  });
+    beforeEach(() => {
+        console.log('before each');
+    })
 
-  test("parse simple URL", () => {
-    const parsedUrl = Utils.parseUrl("http://localhost:8080/login");
-    expect(parsedUrl.href).toBe("http://localhost:8080/login");
-    expect(parsedUrl.port).toBe("8080");
-    expect(parsedUrl.protocol).toBe("http:");
-    expect(parsedUrl.query).toEqual({});
-  });
+    beforeAll(() => {
+        console.log('before all');
+    })
 
-  test("parse URL with query", () => {
-    const parsedUrl = Utils.parseUrl(
-      "http://localhost:8080/login?user=user&password=password"
-    );
-    expect(parsedUrl.query).toEqual({
-      user: "user",
-      password: "password",
+    test('first test', () => {
+        const result = Utils.toUpperCase('abc');
+        expect(result).toBe('ABC');
     });
-  });
+    test('parse simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    });
 
-  test("test invalid URL", () => {
-    function expectError() {
-      Utils.parseUrl("");
-    }
-    expect(expectError).toThrowError();
-  });
+    test('parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(expectedQuery).toBe(expectedQuery);
+    })
 
-  test("test invalid URL with try/catch", () => {
-    try {
-      Utils.parseUrl("");
-    } catch (error) {
-      expect(error).toBeInstanceOf(Error);
-      expect(error).toHaveProperty("message", "empty URL");
-    }
-  });
+    test('test invalid URL', () => {
+        function expectError() {
+            Utils.parseUrl('')
+        }
+        expect(expectError).toThrow('Empty url');
+    });
+
+    test('test invalid URL with arrow function', () => {
+        expect(() => {
+            Utils.parseUrl('')
+        }).toThrow('Empty url');
+    });
+
+    test.only('test invalid URL with try catch', () => {
+        try {
+            Utils.parseUrl('');
+        } catch (error) {
+            expect(error).toBeInstanceOf(Error);
+            expect(error).toHaveProperty('message', 'Empty url!');
+        }
+    });
+
 });
