738a22f71814fc5fe56ef4e5be8855d6e9f5d660
Cleaver Barnes <cbarnes@cleaver.ca>
2020-11-11 16:27:17 -0500

    create some sample tests

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,5 +1,50 @@
+import { Utils } from "../app/Utils";
+
 describe("Utils test suite", () => {
+  beforeEach(() => {
+    console.log("before each");
+  });
+
+  beforeAll(() => {
+    console.log("before all");
+  });
+
   test("first test", () => {
-    console.log("tests work");
+    const result = Utils.toUpperCase("abc");
+    expect(result).toBe("ABC");
+  });
+
+  test("parse simple URL", () => {
+    const parsedUrl = Utils.parseUrl("http://localhost:8080/login");
+    expect(parsedUrl.href).toBe("http://localhost:8080/login");
+    expect(parsedUrl.port).toBe("8080");
+    expect(parsedUrl.protocol).toBe("http:");
+    expect(parsedUrl.query).toEqual({});
+  });
+
+  test("parse URL with query", () => {
+    const parsedUrl = Utils.parseUrl(
+      "http://localhost:8080/login?user=user&password=password"
+    );
+    expect(parsedUrl.query).toEqual({
+      user: "user",
+      password: "password",
+    });
+  });
+
+  test("test invalid URL", () => {
+    function expectError() {
+      Utils.parseUrl("");
+    }
+    expect(expectError).toThrowError();
+  });
+
+  test("test invalid URL with try/catch", () => {
+    try {
+      Utils.parseUrl("");
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty("message", "empty URL");
+    }
   });
 });
